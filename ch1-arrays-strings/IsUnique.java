3bf8351d9da57942353b9afadab7cb11b78a8fca
Daniel Leskosky <dleskosky@gmail.com>
2020-11-04 19:22:39 -0500

    Commit for 1.1

--- ch1-arrays-strings/IsUnique.java
+++ ch1-arrays-strings/IsUnique.java
@@ -1,26 +1,26 @@
 public class IsUnique {
-  public static void main(String[] args) {
-    String str = "apple";
-    boolean result = isUniqueChars(str);
-    System.out.println(result);
-  }
-
-  static boolean isUniqueChars(String str) {
-    if (str.length() > 128) {
-      return false;
+    public static void main(String[] args) {
+        String str = "apple";
+        boolean result = isUniqueChars(str);
+        System.out.println(result);
     }
 
-    boolean[] char_set = new boolean[128];
-    for (int i = 0; i < str.length(); i++) {
-      int val = str.charAt(i);
-      if (char_set[val]) {
-        return false;
-      }
+    static boolean isUniqueChars(String str) {
+        if (str.length() > 128) {
+            return false;
+        }
 
-      char_set[val] = true;
+        boolean[] char_set = new boolean[128];
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            if (char_set[val]) {
+                return false;
+            }
+
+        char_set[val] = true;
+        }
+
+        return true;
     }
-
-    return true;
-  }
 
 }

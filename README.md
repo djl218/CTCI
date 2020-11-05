2056d95a9f8886b1440c41c5cbd05b8e2b8f32c5
Daniel Leskosky <dleskosky@gmail.com>
2020-11-04 19:10:08 -0500

    Commit for 1.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cracking the Coding Interview
+
+This repository contains the exercises that I have completed from Cracking the Coding Interview.
---
+++ ch1-arrays-strings/IsUnique.java
@@ -0,0 +1,26 @@
+public class IsUnique {
+  public static void main(String[] args) {
+    String str = "apple";
+    boolean result = isUniqueChars(str);
+    System.out.println(result);
+  }
+
+  static boolean isUniqueChars(String str) {
+    if (str.length() > 128) {
+      return false;
+    }
+
+    boolean[] char_set = new boolean[128];
+    for (int i = 0; i < str.length(); i++) {
+      int val = str.charAt(i);
+      if (char_set[val]) {
+        return false;
+      }
+
+      char_set[val] = true;
+    }
+
+    return true;
+  }
+
+}
